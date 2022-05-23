71e0af8405bde1aef681dcae633687bf0170552e
Eric Anderson <eanders@microsoft.com>
2022-05-23 16:11:08 -0700

    Initial files for MakeCode project

---
+++ ext.ts
@@ -0,0 +1,24 @@
+namespace MakeyMakey {
+    //% blockId=set_makeymakey_controls
+    //% block="Remap simulator keys to Makey Makey defaults"
+    export function setMakeyMakeyInputs() {
+        // Configure input for player 1
+        keymap.setPlayerKeys(
+            1,
+            keymap.KeyCode.UpArrow,
+            keymap.KeyCode.DownArrow,
+            keymap.KeyCode.LeftArrow,
+            keymap.KeyCode.RightArrow,
+            keymap.KeyCode.MouseLeftButton,
+            keymap.KeyCode.Space);
+        // Configure input for player 2
+        keymap.setPlayerKeys(
+            2,
+            keymap.KeyCode.W,
+            keymap.KeyCode.S,
+            keymap.KeyCode.A,
+            keymap.KeyCode.D,
+            keymap.KeyCode.F,
+            keymap.KeyCode.G);
+    }
+}
