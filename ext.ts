e21756834a0f70ef37be51f564f0b48a6be9033d
wmacfarl <wmacfarl@gmail.com>
2022-06-15 15:42:53 -0400

    fork sample repo

--- ext.ts
+++ ext.ts
@@ -21,4 +21,28 @@ namespace MakeyMakey {
             keymap.KeyCode.F,
             keymap.KeyCode.G);
     }
+
+    //% blockId=set_key_to_button
+    //% block="Set key to button"
+    export function setMakeyMakeyInputs(key, button) {
+        // Configure input for player 1
+        console.log("keymap", keymap)
+        keymap.setPlayerKeys(
+            1,
+            keymap.KeyCode.UpArrow,
+            keymap.KeyCode.DownArrow,
+            keymap.KeyCode.LeftArrow,
+            keymap.KeyCode.RightArrow,
+            keymap.KeyCode.MouseLeftButton,
+            keymap.KeyCode.Space);
+        // Configure input for player 2
+        keymap.setPlayerKeys(
+            2,
+            keymap.KeyCode.W,
+            keymap.KeyCode.S,
+            keymap.KeyCode.A,
+            keymap.KeyCode.D,
+            keymap.KeyCode.F,
+            keymap.KeyCode.G);
+    }
 }
